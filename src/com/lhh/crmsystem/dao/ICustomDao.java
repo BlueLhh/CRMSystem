8f062b784c00455db777498d27bc6127728c457b
Lan <lanhonghui100@gmail>
2017-12-10 13:40:46 +0800

    完成客户的基础增删改查DAO层

--- src/com/lhh/crmsystem/dao/ICustomDao.java
+++ src/com/lhh/crmsystem/dao/ICustomDao.java
@@ -1,5 +1,47 @@
 package com.lhh.crmsystem.dao;
 
+import com.lhh.crmsystem.entity.Custom;
+
 public interface ICustomDao {
+
+	/**
+	 * 插入一个新的客户
+	 * 
+	 * @param custom
+	 * @return
+	 */
+	public int insert(Custom custom);
+
+	/**
+	 * 通过一个ID删除员工
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 修改客户信息
+	 * 
+	 * @param custom
+	 * @return
+	 */
+	public int updateByObj(Custom custom);
+
+	/**
+	 * 通过ID来查找一个客户信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Custom queryById(Integer id);
+
+	/**
+	 * 动态查询（条件查询）
+	 * 
+	 * @param custom
+	 * @return
+	 */
+	public Custom queryByObj(Custom custom);
 
 }
