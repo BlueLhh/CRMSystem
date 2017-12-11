9982bf4bfef4119718c0a1afa3d9ba908b502f88
Lan <lanhonghui100@gmail>
2017-12-11 12:59:46 +0800

    完成Rights基础增删改查dao层设计

--- src/com/lhh/crmsystem/dao/IRightsDao.java
+++ src/com/lhh/crmsystem/dao/IRightsDao.java
@@ -1,5 +1,46 @@
 package com.lhh.crmsystem.dao;
 
-public interface IRightsDao {
+import java.util.List;
 
+import com.lhh.crmsystem.entity.Rights;
+
+public interface IRightsDao {
+	/**
+	 * 增
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public int insert(Rights rights);
+
+	/**
+	 * 删
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 改
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public Rights updateByObj(Rights rights);
+
+	/**
+	 * 查
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Rights queryById(Integer id);
+
+	/**
+	 * 查询全部的信息
+	 * 
+	 * @return
+	 */
+	public List<Rights> queryByAll();
 }
