19f589c5da70fc096c43f4efc88eed3aad820059
Lan <lanhonghui100@gmail>
2017-12-11 12:57:50 +0800

    完成CustomInfo基础增删改查dao层设计

--- src/com/lhh/crmsystem/dao/ICustomInfoDao.java
+++ src/com/lhh/crmsystem/dao/ICustomInfoDao.java
@@ -1,5 +1,46 @@
 package com.lhh.crmsystem.dao;
 
-public interface ICustomInfoDao {
+import java.util.List;
 
+import com.lhh.crmsystem.entity.CustomInfo;
+
+public interface ICustomInfoDao {
+	/**
+	 * 增
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public int insert(CustomInfo customInfo);
+
+	/**
+	 * 删
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 改
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public CustomInfo updateByObj(CustomInfo customInfo);
+
+	/**
+	 * 查
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public CustomInfo queryById(Integer id);
+
+	/**
+	 * 查询全部的信息
+	 * 
+	 * @return
+	 */
+	public List<CustomInfo> queryByAll();
 }
