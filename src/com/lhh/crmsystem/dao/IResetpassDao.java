14b9a21d11ea6ed73ef14501e552709ec64ca2b1
Lan <lanhonghui100@gmail>
2017-12-11 12:59:14 +0800

    完成Resetpass基础增删改查dao层设计

--- src/com/lhh/crmsystem/dao/IResetpassDao.java
+++ src/com/lhh/crmsystem/dao/IResetpassDao.java
@@ -1,5 +1,46 @@
 package com.lhh.crmsystem.dao;
 
-public interface IResetpassDao {
+import java.util.List;
 
+import com.lhh.crmsystem.entity.Resetpass;
+
+public interface IResetpassDao {
+	/**
+	 * 增
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public int insert(Resetpass resetpass);
+
+	/**
+	 * 删
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 改
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public Resetpass updateByObj(Resetpass resetpass);
+
+	/**
+	 * 查
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Resetpass queryById(Integer id);
+
+	/**
+	 * 查询全部的信息
+	 * 
+	 * @return
+	 */
+	public List<Resetpass> queryByAll();
 }
