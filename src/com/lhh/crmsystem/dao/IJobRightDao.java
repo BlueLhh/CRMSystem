cc7268cd07eac7e4dd28d2b705e9bff4ec46da1c
Lan <lanhonghui100@gmail>
2017-12-11 12:58:35 +0800

    完成JobRight基础增删改查dao层设计

--- src/com/lhh/crmsystem/dao/IJobRightDao.java
+++ src/com/lhh/crmsystem/dao/IJobRightDao.java
@@ -1,5 +1,46 @@
 package com.lhh.crmsystem.dao;
 
-public interface IJobRightDao {
+import java.util.List;
 
+import com.lhh.crmsystem.entity.JobRight;
+
+public interface IJobRightDao {
+	/**
+	 * 增
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public int insert(JobRight jobRight);
+
+	/**
+	 * 删
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 改
+	 * 
+	 * @param customInfo
+	 * @return
+	 */
+	public JobRight updateByObj(JobRight jobRight);
+
+	/**
+	 * 查
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public JobRight queryById(Integer id);
+
+	/**
+	 * 查询全部的信息
+	 * 
+	 * @return
+	 */
+	public List<JobRight> queryByAll();
 }
