73a5653b24de698532d264962177daba842c298f
Lan <lanhonghui100@gmail>
2017-12-11 10:16:14 +0800

    完成部门的基础增删改查等dao层设计

--- src/com/lhh/crmsystem/dao/IDepartmentDao.java
+++ src/com/lhh/crmsystem/dao/IDepartmentDao.java
@@ -1,5 +1,46 @@
 package com.lhh.crmsystem.dao;
 
-public interface IDepartmentDao {
+import java.util.List;
 
+import com.lhh.crmsystem.entity.Department;
+
+public interface IDepartmentDao {
+	/**
+	 * 插入一条部门信息
+	 * 
+	 * @param department
+	 * @return
+	 */
+	public int insert(Department department);
+
+	/**
+	 * 删除一条部门信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 更新一条部门信息
+	 * 
+	 * @param department
+	 * @return
+	 */
+	public Department updateByObj(Department department);
+
+	/**
+	 * 查询一条部门信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Department queryById(Integer id);
+
+	/**
+	 * 查询全部部门信息
+	 * 
+	 * @return
+	 */
+	public List<Department> queryByAll();
 }
