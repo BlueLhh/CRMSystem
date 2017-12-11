deba49c75ee8a88794be9c3890ace0e8fcecffef
Lan <lanhonghui100@gmail>
2017-12-11 12:56:33 +0800

    完成ConsultRecord增删改查基础dao层设计

--- src/com/lhh/crmsystem/dao/IConsultRecordDao.java
+++ src/com/lhh/crmsystem/dao/IConsultRecordDao.java
@@ -1,5 +1,47 @@
 package com.lhh.crmsystem.dao;
 
+import java.util.List;
+
+import com.lhh.crmsystem.entity.ConsultRecord;
+
 public interface IConsultRecordDao {
 
+	/**
+	 * 增
+	 * 
+	 * @param consultRecord
+	 * @return
+	 */
+	public int insert(ConsultRecord consultRecord);
+
+	/**
+	 * 删
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(Integer id);
+
+	/**
+	 * 改
+	 * 
+	 * @param consultRecord
+	 * @return
+	 */
+	public ConsultRecord updateByObj(ConsultRecord consultRecord);
+
+	/**
+	 * 查
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ConsultRecord queryById(Integer id);
+
+	/**
+	 * 查询全部的信息
+	 * 
+	 * @return
+	 */
+	public List<ConsultRecord> queryByAll();
 }
